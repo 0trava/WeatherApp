52f22d1d0ea08dd369006353f9599f176f36409a
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-11 14:42:11 +0300

    add first test page

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Loading/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color:'white',
-    fontSize: 24,
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'black',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     color:'white',
+//     fontSize: 24,
 
-  }
-});
+//   }
+// });
---
+++ Loading.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Loading weather status...</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#FDF2A0",
+    },
+
+    text: {
+        color: "#2c2c2c",
+        fontSize: 28,
+
+    }
+})
