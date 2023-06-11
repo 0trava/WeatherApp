c8059885fafdba8806fc75bcb8df0162208ea0ae
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-11 15:37:06 +0300

    add  requestPermissions

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Loading from './Loading';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+
 import React from 'react';
+import * as Location from 'expo-location';
+
+import Loading from './Loading';
 
 export default class extends React.Component {
 
-  
+  getLocation = async () => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+      
+    } catch (error) {
+      Alert.alert('Can`t get your location');
+      
+    }
+  }
 
-
-
-
+  componentDidMount() {
+    this.getLocation();
+  }
 
   render () {
     return ( 
